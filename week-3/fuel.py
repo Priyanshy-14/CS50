5936dae8abf74a05eedf92c34409777ba17cd045
Priyanshy-14 <priyanshyyy1425@gmail.com>
2025-06-07 22:52:02 +0530

    week-3 assignment

---
+++ week-3/fuel.py
@@ -0,0 +1,27 @@
+def main():
+    while True:
+
+        try:
+            div = input("Fraction:")
+            x, y = div.split("/")
+            x=int(x)
+            y= int(y)
+            percent = (x*100)/y
+
+            if percent<= 1:
+                print("E")
+            elif percent >= 99:
+                print("F")
+            else:
+                print(f"{round(percent)}%")
+
+        except ValueError:
+            continue
+        except ZeroDivisionError:
+            continue
+
+        break
+
+
+main()
+
