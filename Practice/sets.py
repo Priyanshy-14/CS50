c87bf2b1dbf169631a9fbf218bf1439c83dbaf65
Priyanshy-14 <priyanshyyy1425@gmail.com>
2025-06-19 22:11:03 +0530

    Practice set

---
+++ Practice/sets.py
@@ -0,0 +1,12 @@
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8]
+set1 = set(list1)
+set2 = set(list2)
+intersection = set1 & set2
+union = set1 | set2
+symmetric_difference = set1 ^ set2
+print("List 1:", list1)
+print("List 2:", list2)
+print("Intersection:", intersection)
+print("Union:", union)
+print("Symmetric Difference:", symmetric_difference)
